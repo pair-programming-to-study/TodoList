2417acfac9dc09746f833bb43294f80fbb3480fc
이재하 <jhl2619@naver.com>
2022-08-14 22:38:56 +0900

    feat: Add auth api

---
+++ client/src/apis/signIn.ts
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import { SERVER_URL } from 'utils/constants';
+
+export interface SignInRequest {
+  email: string;
+  password: string;
+}
+
+export interface SignInResponse {
+  access_token: string;
+}
+
+export const signIn = async ({ email, password }: SignInRequest) => {
+  const response = await axios.post<SignInResponse>(
+    `${SERVER_URL}/auth/signin`,
+    {
+      email,
+      password,
+    }
+  );
+  return response.data;
+};
---
+++ client/src/apis/signUp.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { SERVER_URL } from 'utils/constants';
+
+export interface SignUpRequest {
+  email: string;
+  password: string;
+}
+
+export interface SignUpResponse {
+  access_token: string;
+}
+
+export const signUp = async ({ email, password }: SignUpRequest) => {
+  const response = await axios.post<SignUpResponse>(
+    `${SERVER_URL}/auth/signup`,
+    {
+      email,
+      password,
+    }
+  );
+
+  return response.data;
+};
