bb61bd96b98496893f5a5d50a1daab03ed4105f0
이재하 <jhl2619@naver.com>
2022-08-18 03:31:31 +0900

    feat: Implement sign in page css

--- client/src/components/signIn/SignInForm/SignInForm.tsx
+++ client/src/components/signIn/SignInForm/SignInForm.tsx
@@ -1,4 +1,9 @@
+import { Button, Input } from 'components/common';
+import DividerLine from 'components/common/DriverLine/DriverLine';
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Path from 'routes/Path';
+import styled from 'styled-components';
 
 export interface SignInFormProps {
   email: string;
@@ -16,11 +21,55 @@ function SignInForm({
   onSignIn,
 }: SignInFormProps) {
   return (
-    <form onSubmit={onSignIn}>
-      <input type="email" value={email} onChange={onChangeEmail} />
-      <input type="password" value={password} onChange={onChangePassword} />
-      <button type="submit">Signin</button>
-    </form>
+    <FormWrapper onSubmit={onSignIn}>
+      <Input
+        type="email"
+        width={300}
+        height={40}
+        value={email}
+        placeholder="이메일을 입력해주세요."
+        onChange={onChangeEmail}
+      />
+      <Input
+        type="password"
+        width={300}
+        height={40}
+        value={password}
+        placeholder="비밀번호를 입력해주세요."
+        onChange={onChangePassword}
+      />
+
+      <Button width={300} height={40} variant="primary" type="submit">
+        로그인
+      </Button>
+
+      <DividerLine width="100%" color="#d4d2cf" mt={10} mb={30} />
+
+      <Text>
+        아직 회원이 아니신가요?
+        <StyledLink to={Path.SIGNUP}>회원가입 하기</StyledLink>
+      </Text>
+    </FormWrapper>
   );
 }
+
+const FormWrapper = styled.form`
+  display: flex;
+  flex-direction: column;
+  input {
+    margin-bottom: 10px;
+  }
+  button {
+    margin-bottom: 20px;
+  }
+`;
+
+const Text = styled.span`
+  font-size: 14px;
+`;
+
+const StyledLink = styled(Link)`
+  color: #4848e9;
+  margin-left: 6px;
+`;
 export default SignInForm;
