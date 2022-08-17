f5fe1c625a6d5c2bcf5949dc5aceec1b62d39b81
이재하 <jhl2619@naver.com>
2022-08-18 03:31:47 +0900

    feat: Implement sign up page css

--- client/src/components/signUp/SignUpForm/SignUpForm.tsx
+++ client/src/components/signUp/SignUpForm/SignUpForm.tsx
@@ -1,5 +1,9 @@
-import { Button } from 'components/common';
+import { Button, Input } from 'components/common';
+import DividerLine from 'components/common/DriverLine/DriverLine';
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Path from 'routes/Path';
+import styled from 'styled-components';
 
 export interface SignUpFormProps {
   email: string;
@@ -23,30 +27,69 @@ function SignUpForm({
   isDisabled,
 }: SignUpFormProps) {
   return (
-    <form onSubmit={onSignUp}>
-      <input
+    <FormWrapper onSubmit={onSignUp}>
+      <Input
+        width={300}
+        height={40}
         type="email"
+        placeholder="이메일을 입력해주세요"
         value={email}
         onChange={onChangeEmail}
-        placeholder="이메일을 입력해주세요"
       />
-      <input
+      <Input
+        width={300}
+        height={40}
+        placeholder="비밀번호를 입력해주세요."
         type="password"
         value={password}
         onChange={onChangePassword}
-        placeholder="비밀번호를 입력해주세요"
       />
-      <input
+      <Input
+        width={300}
+        height={40}
         type="password"
+        placeholder="비밀번호를 다시 입력해주세요."
         value={passwordConfirm}
         onChange={onChangePasswordConfirm}
-        placeholder="비밀번호를 다시 입력해주세요"
       />
-      <Button type="submit" variant="primary" disabled={isDisabled}>
-        SignUp
+      <Button
+        width={300}
+        height={40}
+        variant="primary"
+        type="submit"
+        disabled={isDisabled}
+      >
+        회원가입
       </Button>
-    </form>
+
+      <DividerLine width="100%" color="#d4d2cf" mt={10} mb={30} />
+
+      <Text>
+        이미 회원이신가요?
+        <StyledLink to={Path.SIGNIN}>로그인 하기</StyledLink>
+      </Text>
+    </FormWrapper>
   );
 }
+
+const FormWrapper = styled.form`
+  display: flex;
+  flex-direction: column;
+  input {
+    margin-bottom: 12px;
+  }
+  button {
+    margin: 15px 0;
+  }
+`;
+
+const Text = styled.span`
+  font-size: 14px;
+`;
+
+const StyledLink = styled(Link)`
+  color: #4848e9;
+  margin-left: 6px;
+`;
 
 export default SignUpForm;
