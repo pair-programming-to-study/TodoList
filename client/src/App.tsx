0d1d69b6fc5344bad0414076f061eef618985d62
이재하 <jhl2619@naver.com>
2022-08-18 00:57:19 +0900

    feat: Reset css by applying global styles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import GlobalStyles from 'libs/styles/globalStyles';
 import Routing from './routes/Routing';
 
 function App() {
   return (
-    <div>
+    <>
+      <GlobalStyles />
       <Routing />
-    </div>
+    </>
   );
 }
 
