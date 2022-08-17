fd8d1891d561366f6af59788880cfcaed8311d7e
이재하 <jhl2619@naver.com>
2022-08-17 23:11:12 +0900

    feat: Desigin todo page

--- client/src/pages/TodoPage.tsx
+++ client/src/pages/TodoPage.tsx
@@ -1,7 +1,35 @@
+import TodoContainer from 'containers/todo/TodoContainer';
 import React from 'react';
+import styled from 'styled-components';
 
 function TodoPage() {
-  return <div>TodoPage</div>;
+  return (
+    <Container>
+      <Inner>
+        <TodoH1>Todo</TodoH1>
+        <TodoContainer />
+      </Inner>
+    </Container>
+  );
 }
+
+const Container = styled.main`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  font-size: 14px;
+  margin-top: 40px;
+`;
+
+const Inner = styled.div`
+  width: 500px;
+`;
+
+const TodoH1 = styled.h1`
+  font-size: 24px;
+  font-weight: bold;
+  margin-bottom: 20px;
+`;
 
 export default TodoPage;
